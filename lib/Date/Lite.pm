3b9842b409a1554d44a013f684e6559a3aeaef54
John Drago <jdrago_999@yahoo.com>
2010-01-01 19:47:10 +0000

    Import of JOHND/Date-Lite-0.000_01 from CPAN.
    gitpan-cpan-distribution: Date-Lite
    gitpan-cpan-version:      0.000_01
    gitpan-cpan-path:         JOHND/Date-Lite-0.000_01.tar.gz
    gitpan-cpan-author:       JOHND
    gitpan-cpan-maturity:     developer

---
+++ lib/Date/Lite.pm
@@ -0,0 +1,37 @@
+
+package Date::Lite;
+
+use strict;
+use warnings 'all';
+
+our $VERSION = '0.000_01';
+
+1;# return true:
+
+=pod
+
+=head1 NAME
+
+Date::Lite - Simple date utility objects and functions.
+
+=head1 SYNOPSIS
+
+  # TBD
+
+=head1 DESCRIPTION
+
+TBD
+
+=head1 AUTHOR
+
+John Drago <jdrago_999@yahoo.com>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright 2010 John Drago all rights reserved.
+
+This software is Free software and may be used and redistributed under the same terms
+as perl itself, any version.
+
+=cut
+
